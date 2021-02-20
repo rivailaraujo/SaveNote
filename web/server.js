adf3dbc3c0a3f186889ad993dabd6c5bb9cec46a
rivail <araujorivail@outlook.com>
2021-02-19 22:25:22 -0300

    iniciando config pra testar cadastro front end

---
+++ web/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+var express = require('express');
+//const app = require('./app');
+const port = process.env.PORT || 3000;
+
+
+
+
+var app = express();
+const server = http.createServer(app);
+server.listen(port);
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// public folder to store assets
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/img'));
+// routes for app
+
+app.get('/', function(req, res) {
+    //console.log("Entrou na rota /")
+    res.render('index.html');
+  
+});
