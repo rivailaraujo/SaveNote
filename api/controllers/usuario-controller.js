56ed670a97c7442042ec5e2c3a9550326d6eb13c
rivail <araujorivail@outlook.com>
2021-02-23 22:34:41 -0300

    ajustes api pra trabalhar com async/promisses

--- api/controllers/usuario-controller.js
+++ api/controllers/usuario-controller.js
@@ -1,182 +1,118 @@
-const mysql = require("../mysql").pool;
+const mysql = require("../mysql");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+exports.postUsuario = async (req, res, next) => {
+    try {
 
-exports.postUsuario = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            return res.status(500).send({
-                error: error,
-            });
-        }
-        conn.query(
-            "SELECT * FROM usuario WHERE email = ?",
-            [req.body.email],
-            (error, resultado, field) => {
-                if (error) {
-                    conn.release();
-                    return res.status(500).send({
-                        error: error,
-                        response: null,
-                    });
-                }
-                if (resultado.length > 0) {
-                    conn.release();
-                    return res.status(409).send({
-                        mensagem: "Usuario Já Cadastrado",
-                    });
-                } else {
-                    bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-                        if (errBcrypt) {
-                            return res.status(500).send({
-                                error: errBcrypt,
-                            });
-                        }
-                        conn.query(
-                            "INSERT INTO usuario (nome, imagem, email, senha, cidade, estado, criado) VALUES(?,'web/public/img', ?, ?, ?, ?, NOW())",
-                            [req.body.nome, req.body.email, hash, req.body.cidade, req.body.estado],
-                            (error, resultado, field) => {
-                                conn.release();
-
-                                if (error) {
-                                    return res.status(500).send({
-                                        error: error,
-                                        response: null,
-                                    });
-                                }
-                                return res.status(201).send({
-                                    mensagem: "Usuario inserido com sucesso",
-                                    id_usuario: resultado.insertId,
-                                });
-                            }
-                        );
-                    });
-                }
-
-            }
+        var response = await mysql.execute(
+            "SELECT * FROM usuario WHERE email = ? AND nome = ?;",
+            [req.body.email, req.body.nome]
         );
-
-    });
-}
-
-exports.loginUsuario = (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            return res.status(500).send({
-                error: error,
+        if (response.length > 0) {
+            return res.status(401).send({
+                mensagem: "Nome e Email ja cadastrado",
             });
         }
-        const query = "SELECT * FROM usuario WHERE email = ?";
-        conn.query(query, [req.body.email], (error, resultado, field)  => {
-            conn.release();
 
-            if (error) {
-                return res.status(500).send({
-                    error: error,
-                    response: null,
-                });
-            }
-
-            if(resultado.length < 1){
-                return res.status(401).send({
-                    mensagem: "Falha na autenticação"
-                });
-            }
-
-            bcrypt.compare(req.body.senha, resultado[0].senha, (err, result) => {
-                if(err){
-                    return res.status(401).send({
-                        mensagem: "Falha na autenticação"
-                    });
-                };
-                if(result){
-                    
-                    let token = jwt.sign({
-                        id_usuario: resultado[0].id_usuario,
-                        email: resultado[0].email
-                    }, process.env.JWT_KEY,{expiresIn: "1h"});
-
-                    conn.query("INSERT INTO sessao (id_usuario, token, data_expiracao, expirado, criado, atualizado) VALUES (?, ?, NULL, 0, NOW(), NULL)", 
-                    [resultado[0].id_usuario, token], (error, result, field)  => {
-                        conn.release();
-                        if (error) {
-                            console.log(resultado[0].id_usuario, token);
-                            res.status(500).send({
-                                
-                                error: error,
-                                response: null,
-                            });
-                        }
-
-                    });
-                    
-
-                    return res.status(200).send({
-                        mensagem: "Autenticado com sucesso",
-                        token: token
-                    });
-                    
-                };
-                return res.status(401).send({
-                    mensagem: "Falha na autenticação"
-                });
-            })
-
-            
-            
-            //res.status(201).send(resultado);
-        });
-    });
-}
-
-exports.logoutUsuario = (req, res, next) => {
-
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            return res.status(500).send({
-                error: error,
+        response = await mysql.execute(
+            "SELECT * FROM usuario WHERE email = ?;",
+            [req.body.email]
+        );
+        if (response.length > 0) {
+            return res.status(401).send({
+                mensagem: "Email ja cadastrado",
             });
         }
-        const query = "UPDATE sessao SET data_expiracao = NOW(), expirado = 1 WHERE sessao.token = ?;";
-        conn.query(query, [req.token], (error, resultado, field)  => {
-            conn.release();
 
-            if (error) {
-                return res.status(500).send({
-                    error: error,
-                    response: null,
-                });
-            }
+        response = await mysql.execute(
+            "SELECT * FROM usuario WHERE nome = ?;",
+            [req.body.nome]
+        );
+        if (response.length > 0) {
+            return res.status(401).send({
+                mensagem: "Nome de usuário ja cadastrado",
+            });
+        } else {
+            const hash = await bcrypt.hashSync(req.body.senha, 10);
 
-            if(resultado){
-                return res.status(401).send({
-                    mensagem: "Logout feito com sucesso"
-                });
-            }        //res.status(201).send(resultado);
+            await mysql.execute(
+                "INSERT INTO usuario (nome, imagem, email, senha, cidade, estado, criado) VALUES(?,'web/public/img', ?, ?, ?, ?, NOW());",
+                [req.body.nome, req.body.email, hash, req.body.cidade, req.body.estado])
+
+            return res.status(201).send({
+                mensagem: "Usuario inserido com sucesso",
+                //id_usuario: resultado.insertId,
+            });
+
+        }
+    } catch (error) {
+        return res.status(500).send({
+            error: error,
         });
-    });
-}
+    }
+};
 
-exports.getUsuarios = (req, res, next) => {
-    console.log(req.usuario);
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            return res.status(500).send({
-                error: error,
+exports.loginUsuario = async (req, res, next) => {
+    try {
+        const response = await mysql.execute(
+            "SELECT * FROM usuario WHERE email = ?;",
+            [req.body.email]
+        );
+        if (response.length < 1) {
+            return res.status(401).send({
+                mensagem: "Falha na autenticação",
             });
         }
-        conn.query("SELECT * FROM `usuario`", (error, resultado, field) => {
-            conn.release();
-
-            if (error) {
-                res.status(500).send({
-                    error: error,
-                    response: null,
-                });
-            }
-            res.status(201).send(resultado);
+        if (await bcrypt.compareSync(req.body.senha, response[0].senha)) {
+            const token = jwt.sign({
+                    id_usuario: response[0].id_usuario,
+                    email: response[0].email,
+                },
+                process.env.JWT_KEY, {
+                    expiresIn: "1h",
+                }
+            );
+            await mysql.execute(
+                "INSERT INTO sessao (id_usuario, token, data_expiracao, expirado, criado, atualizado) VALUES (?, ?, NULL, 0, NOW(), NULL);",
+                [response[0].id_usuario, token]
+            );
+            return res.status(200).send({
+                mensagem: "Login feito com sucesso",
+                token: token
+            });
+        }
+    } catch (error) {
+        return res.status(401).send({
+            mensagem: "Falha na autenticação",
         });
-    });
-}
+    }
+};
+
+exports.logoutUsuario = async (req, res, next) => {
+    try {
+        await mysql.execute("UPDATE sessao SET data_expiracao = NOW(), expirado = 1 WHERE sessao.token = ?;",
+            [req.token]);
+
+        return res.status(200).send({
+            mensagem: "Logout feito com sucesso",
+        });
+    } catch (error) {
+
+        res.status(500).send({
+            error: error,
+        });
+    }
+};
+
+exports.getUsuarios = async (req, res, next) => {
+    try {
+        const response = await mysql.execute("SELECT * FROM usuario;");
+        res.status(201).send(response);
+    } catch (error) {
+
+        res.status(500).send({
+            error: error,
+        });
+    }
+};
--- api/middleware/login.js
+++ api/middleware/login.js
@@ -1,9 +1,21 @@
 const jwt = require('jsonwebtoken');
+const mysql = require("../mysql");
 
-exports.obrigatorio = (req, res, next) => {
+exports.obrigatorio = async (req, res, next) => {
     
     try {
+
         const token = req.headers.authorization.split(' ')[1];
+        const response = await mysql.execute(
+            "SELECT * FROM `sessao` WHERE token = ? AND expirado = 1",
+            [token]
+        );
+        console.log(response)
+        if (response.length > 0) {
+            return res.status(401).send({
+                mensagem: "Falha na autenticação jwt, token expirado",
+            });
+        }
         const decode = jwt.verify(token, process.env.JWT_KEY);
         req.usuario = decode;
         req.token = token;
