824105487981b46098ca36a604c422fc41ed7bd8
Adrielson <adrielferreira28@gmail.com>
2021-03-30 12:52:15 -0300

    listar notebooks do usuario Back-end e Front-end ok

--- api/routes/usuario.js
+++ api/routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mysql = require("../mysql").pool;
 //const bcrypt = require("bcrypt");
 const controllerUsuario = require('../controllers/usuario-controller')
+const controllerDocumento = require('../controllers/documento-controller')
 const login = require("../middleware/login");
 
 router.get("/", login.obrigatorio, controllerUsuario.getUsuario);
@@ -13,5 +14,7 @@ router.post("/login", controllerUsuario.loginUsuario);
 
 router.post("/logout", login.obrigatorio, controllerUsuario.logoutUsuario);
 //router.post("/teste", login, controllerUsuario.loginUsuario);
+
+router.get("/notebooks", login.obrigatorio , controllerDocumento.getNotebooksUsuario);
 
 module.exports = router;
